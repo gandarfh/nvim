d29765e5e3298b51b3a1e3d6dd6d91d395798570
gandarfh <jgdsantosferreira@gmail.com>
2025-06-15 16:19:28 -0300

    feat: update LSP keymaps and simplify LSP initialization

--- lua/nvim/lsp/init.lua
+++ lua/nvim/lsp/init.lua
@@ -1,8 +1,4 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-	return
-end
-
+require("lspconfig")
 require("nvim.lsp.mason")
 require("nvim.lsp.handlers").setup()
 require("nvim.lsp.null-ls")
