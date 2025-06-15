1435c9bcb7b79349524a4ff57fd82724abdbba84
gandarfh <jgdsantosferreira@gmail.com>
2025-06-15 12:51:57 -0300

    feat: add highlights and remove terminal keymaps

--- lua/nvim/toggleterm.lua
+++ lua/nvim/toggleterm.lua
@@ -18,16 +18,15 @@ toggleterm.setup({
 	float_opts = {
 		border = "curved",
 	},
+
+	highlights = {
+		FloatBorder = {
+			guifg = "#4b5263",
+		},
+	},
 })
 
-function _G.set_terminal_keymaps()
-	local opts = { noremap = true }
-	-- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
-end
+function _G.set_terminal_keymaps() end
 
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
